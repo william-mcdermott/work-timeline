6119a26c1eeaab5cb633b6c996fc0a06ca2a5f23
William McDermott <williammcdermott@MacBookPro.lan>
2026-01-20 08:38:08 -0600

    Add timeline component

---
+++ src/app/components/timeline/timeline.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  imports: [],
+  templateUrl: './timeline.html',
+  styleUrl: './timeline.scss',
+})
+export class Timeline {
+
+}
